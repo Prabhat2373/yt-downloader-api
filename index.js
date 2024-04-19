5499d3066f85edecc91f3557d3048a720a877b76
Prabhat2373 <prabhattambe10@gmail.com>
2024-04-19 18:58:48 +0530

    improvement

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-
-
-
 // const express = require('express');
 // const app = express();
 // const ytdl = require('ytdl-core');
@@ -24,7 +21,7 @@
 //         const info = await ytdl.getInfo(url);
 //         console.log('info', info.formats);
 
-//         const quality = resolution 
+//         const quality = resolution
 
 //         // Find the format with the requested resolution
 //         const format = ytdl.chooseFormat(info.formats, { quality });
@@ -33,7 +30,7 @@
 //         }
 
 //         res.setHeader('Content-Disposition', `attachment; filename="${info.videoDetails.title}.mp4"`);
-        
+
 //         // Stream the video directly to the response stream
 //         await pipelineAsync(
 //             ytdl(url, { quality: format.itag }),
@@ -45,22 +42,19 @@
 //         res.status(500).json({ error: 'Failed to download video' });
 //     }
 // });
-  
-
 
 // app.post('/info', async (req, res) => {
 //     const { url } = req.body;
 //     if (!ytdl.validateURL(url)) {
 //       return res.status(400).json({ error: 'Invalid YouTube URL' });
 //     }
-  
+
 //     try {
 //       const info = await ytdl.getInfo(url);
 //       const formatsWithAudio = info.formats.filter(format => format.hasAudio && format.hasVideo);
 
 //       const selectedFormat = formatsWithAudio.find(format => format.resolution === '2160p' || format.resolution === '4320p');
 //       console.log('formatsWithAudio', formatsWithAudio);
-
 
 //       // const formats =  ytdl.filterFormats(info)
 //       // const formats = ytdl.filterFormats(info.formats, 'videoandaudio');
@@ -74,26 +68,24 @@
 //     }
 //   });
 
-
 //   const PORT = process.env.PORT || 8000;
 // app.listen(PORT, () => {
 //   console.log(`Server is running on port ${PORT}`);
 // });
 
-
 // File: server.mjs
 
-import express from 'express';
-import ytdl from 'ytdl-core';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import { pipeline } from 'stream';
-import { promisify } from 'util';
-import dotenv from 'dotenv'
+import express from "express";
+import ytdl from "ytdl-core";
+import bodyParser from "body-parser";
+import cors from "cors";
+import { pipeline } from "stream";
+import { promisify } from "util";
+import dotenv from "dotenv";
 
-import cp from 'child_process';
-import ffmpegPath from 'ffmpeg-static';
-dotenv.config()
+import cp from "child_process";
+import ffmpegPath from "ffmpeg-static";
+dotenv.config();
 const app = express();
 const pipelineAsync = promisify(pipeline);
 
@@ -133,128 +125,150 @@ app.use(cors());
 //     //     res.status(500).json({ error: 'Failed to download video' });
 //     // }
 // });
-const frontendDomain = process.env.ACCESS_DOMAIN_URL || 'https://www.fast4k.com'
-console.log('frontendDomain', frontendDomain)
+const frontendDomain =
+  process.env.ACCESS_DOMAIN_URL || "https://www.fast4k.com";
+console.log("frontendDomain", frontendDomain);
 function authenticateReferer(req, res, next) {
-    const referer = req.headers.origin ||'';
-    // const referer = '';
-    // console.log('refer', req.headers.origin);
+  const referer = req.headers.origin || "";
+  // const referer = '';
+  // console.log('refer', req.headers.origin);
 
-    if (!referer || !referer.startsWith(frontendDomain)) {
-        return res.status(403).json({ message: 'Forbidden: Access denied' });
-    }
+  if (!referer || !referer.startsWith(frontendDomain)) {
+    return res.status(403).json({ message: "Forbidden: Access denied" });
+  }
 
-    // Referer header matches the frontend domain, continue to the next middleware
-    next();
+  // Referer header matches the frontend domain, continue to the next middleware
+  next();
 }
 
+app.post("/info", authenticateReferer, async (req, res) => {
+  const { url } = req.body;
+  if (!ytdl.validateURL(url)) {
+    return res.status(400).json({ error: "Invalid YouTube URL" });
+  }
 
-app.post('/info', authenticateReferer, async (req, res) => {
-    const { url } = req.body;
-    if (!ytdl.validateURL(url)) {
-        return res.status(400).json({ error: 'Invalid YouTube URL' });
-    }
+  // try {
+  const info = await ytdl.getInfo(url);
+  // console.log('infoo',info);
+  const formatsWithAudio = info.formats.filter(
+    (format) => format.hasAudio && format.hasVideo
+  );
 
-    // try {
-        const info = await ytdl.getInfo(url);
-        // console.log('infoo',info);
-        const formatsWithAudio = info.formats.filter(format => format.hasAudio && format.hasVideo);
+  const selectedFormat = formatsWithAudio.find(
+    (format) => format.resolution === "2160p" || format.resolution === "4320p"
+  );
+  // console.log('formatsWithAudio', formatsWithAudio);
 
-        const selectedFormat = formatsWithAudio.find(format => format.resolution === '2160p' || format.resolution === '4320p');
-        // console.log('formatsWithAudio', formatsWithAudio);
+  const audio_formats = ytdl.filterFormats(info.formats, "audioonly");
+  const video_formats = ytdl.filterFormats(info.formats, "video");
 
-        const audio_formats = ytdl.filterFormats(info.formats, 'audioonly');
-        const video_formats = ytdl.filterFormats(info.formats, 'video');
+  const meta = info.videoDetails;
 
-        const meta = info.videoDetails
-
-        res.json({ audio_formats, video_formats,meta });
-    // } catch (error) {
-    //     console.error('Error:', error, "Message", error?.message);
-    //     res.status(500).json({ error: `Failed to fetch video information ${error?.message}` });
-    // }
+  res.json({ audio_formats, video_formats, meta });
+  // } catch (error) {
+  //     console.error('Error:', error, "Message", error?.message);
+  //     res.status(500).json({ error: `Failed to fetch video information ${error?.message}` });
+  // }
 });
 
+app.get("/merge", async (req, res) => {
+  const { url, format: desiredQuality } = req.query;
 
+  const sanitizeFilename = (filename) => {
+    // Replace characters that are not allowed in HTTP headers
+    return filename.replace(/[^\w\d-_.]/g, "_"); // Replace invalid characters with underscores
+  };
 
-app.get('/merge', async (req, res) => {
-    const { url,format:desiredQuality } = req.query;
+  function chooseVideoFormat(formats, desiredQuality) {
+    return formats.find((format) => {
+      return (
+        format.qualityLabel && format.qualityLabel.includes(desiredQuality)
+      );
+    });
+  }
 
-    const sanitizeFilename = (filename) => {
-        // Replace characters that are not allowed in HTTP headers
-        return filename.replace(/[^\w\d-_.]/g, '_'); // Replace invalid characters with underscores
-    };
-
-    function chooseVideoFormat(formats, desiredQuality) {
-        return formats.find(format => {
-          return format.qualityLabel && format.qualityLabel.includes(desiredQuality);
-        });
-      }
-      
-
-    try {
-        if (!url || !ytdl.validateURL(url)) {
-            throw new Error('Invalid YouTube URL');
-        }
-
-        const videoInfo = await ytdl.getInfo(url)
-        const videoFormats = videoInfo.formats; // Filter only video formats
-
-        // Choose the desired video format based on quality label from query parameter
-        const selectedFormat = chooseVideoFormat(videoFormats, desiredQuality);
-
-
-        // res.header('Content-Disposition', `attachment; filename=${videoInfo?.videoDetails?.title}.mp4`);
-        if(selectedFormat && videoInfo.videoDetails.title){
-            const safeFilename = sanitizeFilename(videoInfo.videoDetails.title);
-            res.header('Content-Disposition', `attachment; filename=${safeFilename}.mp4`);
-            const video = ytdl(url, { quality:selectedFormat.itag });
-        const audio = ytdl(url, { filter: 'audioonly', highWaterMark: 1 << 25 });
-
-        const ffmpegProcess = cp.spawn(ffmpegPath, [
-            '-i', 'pipe:3',
-            '-i', 'pipe:4',
-            '-map', '0:v',
-            '-map', '1:a',
-            '-c:v', 'copy',
-            '-c:a', 'libmp3lame',
-            '-crf', '27',
-            '-preset', 'veryfast',
-            '-movflags', 'frag_keyframe+empty_moov',
-            '-f', 'mp4',
-            '-loglevel', 'error',
-            '-'
-        ], {
-            stdio: ['pipe', 'pipe', 'pipe', 'pipe', 'pipe']
-        });
-
-        video.pipe(ffmpegProcess.stdio[3]);
-        audio.pipe(ffmpegProcess.stdio[4]);
-        ffmpegProcess.stdio[1].pipe(res);
-
-        let ffmpegLogs = '';
-
-        ffmpegProcess.stdio[2].on('data', (chunk) => {
-            ffmpegLogs += chunk.toString();
-        });
-
-        ffmpegProcess.on('exit', (exitCode) => {
-            if (exitCode !== 0) {
-                console.log('FFmpeg process exited with code', exitCode);
-                // console.error(ffmpegLogs);
-            }
-        });
-    console.log('downloading...')
-        }
-        // res.header('Content-Disposition', `attachment; filename=fast4k_video.mp4`);
-
-        
-    } catch (error) {
-        console.log('Merge operation failed:', error);
-        res.status(500).send('Merge operation failed');
+  try {
+    if (!url || !ytdl.validateURL(url)) {
+      throw new Error("Invalid YouTube URL");
     }
-});
 
+    const videoInfo = await ytdl.getInfo(url);
+    const videoFormats = videoInfo.formats; // Filter only video formats
+
+    // Choose the desired video format based on quality label from query parameter
+    const selectedFormat = chooseVideoFormat(videoFormats, desiredQuality);
+
+    // res.header('Content-Disposition', `attachment; filename=${videoInfo?.videoDetails?.title}.mp4`);
+    if (selectedFormat && videoInfo.videoDetails.title) {
+      const safeFilename = sanitizeFilename(videoInfo.videoDetails.title);
+    
+      const video = ytdl(url, { quality: selectedFormat.itag });
+      const audio = ytdl(url, { filter: "audioonly", highWaterMark: 1 << 25 });
+
+      const ffmpegProcess = cp.spawn(
+        ffmpegPath,
+        [
+          "-i",
+          "pipe:3",
+          "-i",
+          "pipe:4",
+          "-map",
+          "0:v",
+          "-map",
+          "1:a",
+          "-c:v",
+          "copy",
+          "-c:a",
+          "libmp3lame",
+          "-crf",
+          "27",
+          "-preset",
+          "veryfast",
+          "-movflags",
+          "frag_keyframe+empty_moov",
+          "-f",
+          "mp4",
+          "-loglevel",
+          "error",
+          "-",
+        ],
+        {
+          stdio: ["pipe", "pipe", "pipe", "pipe", "pipe"],
+        }
+      );
+
+      video.pipe(ffmpegProcess.stdio[3]);
+      audio.pipe(ffmpegProcess.stdio[4]);
+      ffmpegProcess.stdio[1].pipe(res);
+
+      let ffmpegLogs = "";
+
+      ffmpegProcess.stdio[2].on("data", (chunk) => {
+        ffmpegLogs += chunk.toString();
+      });
+
+      ffmpegProcess.on("exit", (exitCode) => {
+        if (exitCode !== 0) {
+          console.log("FFmpeg process exited with code", exitCode);
+          // console.error(ffmpegLogs);
+        }
+      });
+      console.log("downloading...");
+
+      res.header(
+        "Content-Disposition",
+        `attachment; filename=${safeFilename}.mp4`
+      );
+    }
+
+    // res.header('Content-Type', 'video/mp4');
+  
+    // res.header('Content-Disposition', `attachment; filename=fast4k_video.mp4`);
+  } catch (error) {
+    console.log("Merge operation failed:", error);
+    res.status(500).send("Merge operation failed");
+  }
+});
 
 // app.get('/',(req,res)=>{
 //   res.setHeader('Content-Type', 'text/html');
@@ -264,12 +278,9 @@ app.get('/merge', async (req, res) => {
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    console.log('Node.js version:', process.version);
-
+  console.log(`Server is running on port ${PORT}`);
+  console.log("Node.js version:", process.version);
 });
-
-
 
 // import ffmpegPath from 'ffmpeg-static';
 // import cp from 'child_process';
@@ -286,9 +297,9 @@ app.listen(PORT, () => {
 // const ytmixer = (link, options = {}) => {
 //   const result = new stream.PassThrough({ highWaterMark: options.highWaterMark || 1024 * 512 });
 //   ytdl.getInfo(link, options).then(info => {
-//       let audioStream = ytdl.downloadFromInfo(info, { ...options, quality: 
+//       let audioStream = ytdl.downloadFromInfo(info, { ...options, quality:
 //   'highestaudio' });
-//       let videoStream = ytdl.downloadFromInfo(info, { ...options, quality: 
+//       let videoStream = ytdl.downloadFromInfo(info, { ...options, quality:
 //   'highestvideo' });
 //       // create the ffmpeg process for muxing
 //       let ffmpegProcess = cp.spawn(ffmpegPath, [
@@ -320,10 +331,8 @@ app.listen(PORT, () => {
 //   };
 
 //   res.setHeader('Content-Disposition', `attachment; filename="${info.videoDetails.title}.mp4"`);
-  
-  
-//   ytmixer('https://youtu.be/WO2b03Zdu4Q?si=yKD8uGSRtAoMraLL').pipe(res);
 
+//   ytmixer('https://youtu.be/WO2b03Zdu4Q?si=yKD8uGSRtAoMraLL').pipe(res);
 
 // })
 
